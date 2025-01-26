7cf1c04a08a90655330944cc14278db6004ad1ef
Areebusman27 <155239399+Areebusman27@users.noreply.github.com>
2025-01-26 08:37:07 +0500

    Add files via upload

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,13 @@
-import LandingPage from "./webpage/page";
+"use client"
+import AdminDashboard from "./admin/page";
+import LoanCalculator from "./loancalculator/page";
+import Login from "./login/page";
+import UserPage from "./loanuser/page";
+import LandingPage from "./landingpage/page";
+
 
 export default function Home() {
+ 
   return (
     <div>
       <LandingPage/>
